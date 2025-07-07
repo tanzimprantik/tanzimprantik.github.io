64ca18981411484106fa0f8cef0e4e682a7d099f
Tanzim Mahmud Prantik <147312295+tanzimprantik@users.noreply.github.com>
2025-07-07 15:45:58 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// main.js (micro‑animation demo)
+document.querySelectorAll('section').forEach(sec=>{
+  const obs=new IntersectionObserver(([e])=>{
+    e.target.classList.toggle('reveal',e.isIntersecting)
+  },{threshold:.1});
+  obs.observe(sec);
+});
 
